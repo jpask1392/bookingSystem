ebab3e5026b19285d75a1d3415ffd6de9f28e896
Jamie Pask <jamiepask1392@gmail.com>
2019-07-09 17:44:05 -0700

    Backend Express application added to co API calls Package file updated to include server file

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const PORT = 3001
+
+app.route('/users') 
+	.get((req, res) => {
+		res.json([{
+		  	id: 1,
+		  	username: "Jamie"
+		  }, {
+		  	id: 2,
+		  	username: "Josh"
+		  }]);
+	})
+
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+})
